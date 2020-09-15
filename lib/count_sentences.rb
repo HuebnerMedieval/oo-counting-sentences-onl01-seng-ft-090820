1561b2b5e7a01b0a124cdfc76804062f8543e558
Eli Huebner <elimhuebner@gmail.com>
2020-09-15 18:00:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,9 +17,12 @@ class String
   def count_sentences
     sentences_ar = self.split ("")
     sentence_count = 0
-    sentences_ar.each do |char|
+    sentences_ar.each_with_index do |char, index|
       if char == "." || char == "?" || char == "!"
         sentence_count += 1
+        if sentences_ar[index] == sentences_ar[index + 1]
+          sentence_count -= 1
+        end
       end
     end
     sentence_count
